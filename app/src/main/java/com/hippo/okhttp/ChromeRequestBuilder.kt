bc3f5330801f4f9b165424e32450ba2689af1d71
vvb2060 <vvb2060@gmail.com>
2024-10-26 18:40:06 +0800

    Use desktop UA by default

--- app/src/main/java/com/hippo/okhttp/ChromeRequestBuilder.kt
+++ app/src/main/java/com/hippo/okhttp/ChromeRequestBuilder.kt
@@ -19,7 +19,7 @@ import com.hippo.ehviewer.Settings
 import com.hippo.ehviewer.util.WebViewVersion
 import okhttp3.Request
 
-val CHROME_USER_AGENT = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$WebViewVersion.0.0.0 Mobile Safari/537.36"
+val CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$WebViewVersion.0.0.0 Safari/537.36"
 private const val CHROME_ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
 private const val CHROME_ACCEPT_LANGUAGE = "en-US,en;q=0.9"
 
